6ee69b68dd2e1bc5aa313e417e2ff18cd9fd713d
Ariel Andrade Tintel <arielandrade97@gmail.com>
2022-06-29 22:38:54 -0300

    Projeto inicializado

---
+++ src/main/java/br/com/arieltintel/apibusca_endereco/ApiBuscaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arieltintel.apibusca_endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBuscaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBuscaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arieltintel/apibusca_endereco/ApiBuscaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arieltintel.apibusca_endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBuscaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
